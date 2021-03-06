5e2a676456d731f40749396e0d72f9b9b5dc6a86
javiemgz <javiemgz@gmail.com>
2021-03-06 19:48:07 -0300

    update to styles

--- shared/globals.ts
+++ shared/globals.ts
@@ -1,12 +1,18 @@
 import { css } from '@emotion/react'
 
 export const globalStyles = css`
+  :root {
+    --page-background: #222;
+    --page-text: #ddd;
+  }
   html,
   body {
     padding: 0;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: var(--page-background);
+    color: var(--page-text);
   }
   a {
     color: inherit;
@@ -16,4 +22,5 @@ export const globalStyles = css`
   *::after,
   *::before {
     box-sizing: border-box;
-  }`
+  }
+`
